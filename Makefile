700655c0bba66d00e545f789bbccbc653b61e6c0
dragomir003 <dragolekovic@gmail.com>
2023-04-28 16:44:07 +0200

    Added program installations

--- Makefile
+++ Makefile
@@ -1,6 +1,34 @@
+USER_NAME=dragomir
 
-all:
+BASE_PACKAGE_LIST=awesome git stow zsh cmake gcc clang llvm rustup clangd polybar dmenu firefox nitrogen alacritty starship
+FONTS_PACMAN=ttf-fira-code 
+
+FONTS_PARU=ttf-symbola ttf-nerd-fonts-symbols-1000-em-mono 
+
+CUDA_PACKAGE_LIST=cuda cudnn
+
+all configs pacman:
+
+configs:
+	rm ~/.bashrc ~/.bash_profile
 	stow --verbose --target=$$HOME --restow */
+
+pacman:
+	sudo pacman -S $(BASE_PACKAGE_LIST) $(FONTS_PACMAN)
+	sudo chsh $(USER_NAME) -s /usr/bin/zsh
+	echo "You should log out and than come back to continue installation"
+
+cargo:
+	rustup update stable
+	rustup component add rust-analyzer
+	cargo install paru bob-nvim
+	paru -S $(FONTS_PARU)
+	bob-nvim install latest
+	bob-nvim use latest
+	nvim +PackerInstall
+
+cuda:
+	sudo pacman -S $(CUDA_PACKAGE_LIST)
 
 delete:
 	stow --verbose --target=$$HOME --delete */
--- config/.config/nvim/plugin/packer_compiled.lua
+++ config/.config/nvim/plugin/packer_compiled.lua
@@ -130,7 +130,7 @@ _G.packer_plugins = {
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["true-zen.nvim"] = {
-    config = { "\27LJ\2\nh\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17integrations\1\0\0\1\0\2\flualine\2\rtwilight\2\nsetup\rtrue-zen\frequire\0" },
+    config = { "\27LJ\2\nh\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17integrations\1\0\0\1\0\2\rtwilight\2\flualine\2\nsetup\rtrue-zen\frequire\0" },
     loaded = true,
     path = "/home/dragomir/.local/share/nvim/site/pack/packer/start/true-zen.nvim",
     url = "https://github.com/Pocco81/true-zen.nvim"
@@ -150,7 +150,7 @@ try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B
 time([[Config for twilight.nvim]], false)
 -- Config for: true-zen.nvim
 time([[Config for true-zen.nvim]], true)
-try_loadstring("\27LJ\2\nh\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17integrations\1\0\0\1\0\2\flualine\2\rtwilight\2\nsetup\rtrue-zen\frequire\0", "config", "true-zen.nvim")
+try_loadstring("\27LJ\2\nh\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\17integrations\1\0\0\1\0\2\rtwilight\2\flualine\2\nsetup\rtrue-zen\frequire\0", "config", "true-zen.nvim")
 time([[Config for true-zen.nvim]], false)
 if should_profile then save_profiles() end
 
