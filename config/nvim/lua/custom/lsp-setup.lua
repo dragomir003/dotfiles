52c1a91d786d9f20e0790ab244cba0fa07d9dc9d
dragomir003 <dragolekovic@gmail.com>
2024-08-30 21:09:14 +0200

    Meson now setups new servers

--- config/nvim/lua/custom/lsp-setup.lua
+++ config/nvim/lua/custom/lsp-setup.lua
@@ -5,6 +5,15 @@ local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
+require("mason-lspconfig").setup_handlers {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function (server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup {}
+	end,
+}
+
 lspconfig.lua_ls.setup({
 	single_file_support = true,
 	flags = {
