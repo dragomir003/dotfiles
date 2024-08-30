8a07884b599817ebba400f1a7a8046291304a660
dragomir003 <dragolekovic@gmail.com>
2024-08-30 21:20:28 +0200

    Added automatic script detection

--- config/nvim/lua/base/files.lua
+++ config/nvim/lua/base/files.lua
@@ -5,5 +5,15 @@ vim.filetype.add({
 		m = 'matlab',
 		typ = 'typst',
 		sh = 'bash',
+	},
+	filename = {
+		['*'] = function (path, buf)
+			local fline = vim.api.nvim_buf_get_lines(buf, 0, 1, true)[0]
+			if string.find(fline, "bash") or string.find(fline, "sh") then
+				return "bash"
+			elseif string.find(fline, "zsh") then
+				return "zsh"
+			end
+		end
 	}
 })
